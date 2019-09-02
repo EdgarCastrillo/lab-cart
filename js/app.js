08268a64a455d9004c98ee9828628fd08231324a
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-02 17:56:09 +0200

    empty local storage

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ function loadEventListeners() {
 
   // Empty cart
   emptyCartBtn.addEventListener('click', emptyCart)
+
+  // When loading the document show the local sorage
+  document.addEventListener('DOMContentLoaded', readLocalStorage)
 }
 
 
@@ -46,6 +49,7 @@ function readDataCourse(course) {
 
 // Show the selected course in the cart
 function addToCart(course) {
+  // Template course
   const row = document.createElement('tr')
   row.innerHTML = `
     <td>
@@ -64,19 +68,26 @@ function addToCart(course) {
 // Delete the course in the DOM
 function deleteCourse(e) {
   e.preventDefault()
-  
   let course
+  let courseId
+
   if(e.target.classList.contains('delete-course')) {
     e.target.parentElement.parentElement.remove()
+    course = e.target.parentElement.parentElement
+    courseId = course.querySelector('a').getAttribute('data-id')
   }
+  deleteCourseLocalStorage(courseId)
 }
 
 // Empty cart courses at DOM
 function emptyCart() {
-
   while(courseList.firstChild) {
     courseList.removeChild(courseList.firstChild)
   }
+  
+  // Empty local storage
+  emptyLocalStorage()
+  
   return false
 }
 
@@ -102,4 +113,46 @@ function getCoursesLocalStorage() {
   }
   return coursesLocalStorage
   
+}
+
+// Show local storage courses in the cart
+function readLocalStorage() {
+  let coursesLocalStorage
+  coursesLocalStorage = getCoursesLocalStorage()
+
+  coursesLocalStorage.forEach((course) => {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>
+        <img src="${course.image}" width=150>
+      </td>
+      <td>${course.title}</td>
+      <td>${course.price}</td>
+      <td>
+        <a href="#" class="delete-course" data-id="${course.id}">X</a>
+      </td>
+    `
+    courseList.appendChild(row)
+  })
+}
+
+
+// Delete course with the ID in local storage
+function deleteCourseLocalStorage(course) {
+  let coursesLocalStorage
+  // Get courses array
+  coursesLocalStorage = getCoursesLocalStorage()
+  // Iterate comparing the id of the selected course with those of the local storage
+  coursesLocalStorage.forEach(function(courseLocalStorage, index) {
+    if(courseLocalStorage.id === course) {
+      coursesLocalStorage.splice(index, 1)
+    }
+  })
+  // Add actual array to storage
+  localStorage.setItem('courses', JSON.stringify(coursesLocalStorage))
+}
+
+
+function emptyLocalStorage() {
+  localStorage.clear()
 }
